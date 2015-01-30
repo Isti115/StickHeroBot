9b1e2ad17ea1ddafd2ffc672a593153c1cdb4ffc
István Donkó <istvan.donko+github@gmail.com>
2015-01-30 16:38:46 +0100

    Sometimes works (no repeat)

--- StickHeroBot/StickHeroBot/MainForm.Designer.cs
+++ StickHeroBot/StickHeroBot/MainForm.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.findWindowButton = new System.Windows.Forms.Button();
             this.captureButton = new System.Windows.Forms.Button();
+            this.analyzeButton = new System.Windows.Forms.Button();
+            this.doItButton = new System.Windows.Forms.Button();
+            this.allInOneButton = new System.Windows.Forms.Button();
+            this.repeatCheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // findWindowButton
@@ -52,16 +56,61 @@
             this.captureButton.UseVisualStyleBackColor = true;
             this.captureButton.Click += new System.EventHandler(this.captureButton_Click);
             // 
+            // analyzeButton
+            // 
+            this.analyzeButton.Location = new System.Drawing.Point(213, 13);
+            this.analyzeButton.Name = "analyzeButton";
+            this.analyzeButton.Size = new System.Drawing.Size(75, 23);
+            this.analyzeButton.TabIndex = 2;
+            this.analyzeButton.Text = "Analyze";
+            this.analyzeButton.UseVisualStyleBackColor = true;
+            this.analyzeButton.Click += new System.EventHandler(this.analyzeButton_Click);
+            // 
+            // doItButton
+            // 
+            this.doItButton.Location = new System.Drawing.Point(295, 13);
+            this.doItButton.Name = "doItButton";
+            this.doItButton.Size = new System.Drawing.Size(75, 23);
+            this.doItButton.TabIndex = 3;
+            this.doItButton.Text = "Do It";
+            this.doItButton.UseVisualStyleBackColor = true;
+            this.doItButton.Click += new System.EventHandler(this.doItButton_Click);
+            // 
+            // allInOneButton
+            // 
+            this.allInOneButton.Location = new System.Drawing.Point(377, 13);
+            this.allInOneButton.Name = "allInOneButton";
+            this.allInOneButton.Size = new System.Drawing.Size(75, 23);
+            this.allInOneButton.TabIndex = 4;
+            this.allInOneButton.Text = "All in One";
+            this.allInOneButton.UseVisualStyleBackColor = true;
+            this.allInOneButton.Click += new System.EventHandler(this.allInOneButton_Click);
+            // 
+            // repeatCheckBox
+            // 
+            this.repeatCheckBox.AutoSize = true;
+            this.repeatCheckBox.Location = new System.Drawing.Point(458, 17);
+            this.repeatCheckBox.Name = "repeatCheckBox";
+            this.repeatCheckBox.Size = new System.Drawing.Size(61, 17);
+            this.repeatCheckBox.TabIndex = 5;
+            this.repeatCheckBox.Text = "Repeat";
+            this.repeatCheckBox.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(545, 46);
+            this.Controls.Add(this.repeatCheckBox);
+            this.Controls.Add(this.allInOneButton);
+            this.Controls.Add(this.doItButton);
+            this.Controls.Add(this.analyzeButton);
             this.Controls.Add(this.captureButton);
             this.Controls.Add(this.findWindowButton);
             this.Name = "MainForm";
             this.Text = "StickHeroBot";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +118,10 @@
 
         private System.Windows.Forms.Button findWindowButton;
         private System.Windows.Forms.Button captureButton;
+        private System.Windows.Forms.Button analyzeButton;
+        private System.Windows.Forms.Button doItButton;
+        private System.Windows.Forms.Button allInOneButton;
+        private System.Windows.Forms.CheckBox repeatCheckBox;
     }
 }
 
--- StickHeroBot/StickHeroBot/MainForm.cs
+++ StickHeroBot/StickHeroBot/MainForm.cs
@@ -18,6 +18,14 @@ namespace StickHeroBot
         public Rectangle area = new Rectangle();
         Bitmap bmp;
 
+        int d;
+
+        static Timer t = new Timer();
+        static Timer rt = new Timer();
+
+        //object sndr;
+        //EventArgs ue;
+
         public MainForm()
         {
             InitializeComponent();
@@ -36,12 +44,94 @@ namespace StickHeroBot
             bmp = new Bitmap(area.Width, area.Height);
             Graphics graphics = Graphics.FromImage(bmp);
             graphics.CopyFromScreen(area.X, area.Y, 0, 0, area.Size);
+
+            bmp.Save(@"C:\Users\isti\Desktop\test.png", System.Drawing.Imaging.ImageFormat.Png);
         }
 
+        private void analyzeButton_Click(object sender, EventArgs e)
+        {
+            int b = 10;
+
+            while (bmp.GetPixel(b, 470).GetBrightness() == 0)
+            {
+                b++;
+            }
+
+            int x = b;
+
+            while (bmp.GetPixel(x, 470).R < 230 || bmp.GetPixel(x, 470).G > 50 || bmp.GetPixel(x, 470).B > 50)
+            {
+                x++;
+            }
+
+            d = x - b;
+            doItButton.Text = d.ToString();
+        }
+
+        private void doItButton_Click(object sender, EventArgs e)
+        {
+            t.Interval = 3 * d; //(int)(2.7 * (float)d);
+            t.Tick += new EventHandler(timer_done);
+            mouseDown(area.X + 100, area.Y + 100);
+            t.Start();
+        }
+
+        private static void timer_done(Object myObject, EventArgs myEventArgs)
+        {
+            mouseUp();
+            t.Stop();
+        }
+
+        // copy-pasted window search
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         public static extern IntPtr FindWindow(string strClassName, string strWindowName);
 
         [DllImport("user32.dll")]
         public static extern bool GetWindowRect(IntPtr hwnd, ref Rectangle rectangle);
+
+        // copy-pasted mouse click
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, uint dwExtraInf);
+        public static void mouseDown(int x, int y)
+        {
+            Cursor.Position = new Point(x, y);
+            mouse_event(0x0002, 0, 0, 0, 0); //make left button down
+        }
+
+        public static void mouseUp()
+        {
+            mouse_event(0x0004, 0, 0, 0, 0); //make left button up
+        }
+
+        private void allInOneButton_Click(object sender, EventArgs e)
+        {
+            rt.Interval = 3000;
+            rt.Tick += new EventHandler(rt_done);
+
+            captureButton_Click(sender, e);
+            analyzeButton_Click(sender, e);
+            doItButton_Click(sender, e);
+
+            //rt.Tick += new EventHandler(captureButton_Click);
+            //rt.Tick += new EventHandler(analyzeButton_Click);
+            //rt.Tick += new EventHandler(doItButton_Click);
+
+            //sndr = sender;
+            //ue = e;
+
+            rt.Start();
+        }
+
+        private static void rt_done(Object myObject, EventArgs myEventArgs)
+        {
+            mouseUp();
+
+            //captureButton_Click(EventArgs.Empty);
+            //analyzeButton_Click(EventArgs.Empty);
+            //doItButton_Click(EventArgs.Empty);
+
+            
+        }
+
     }
 }
